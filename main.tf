0b8ce589aa11d9c8f3ddef17aa998ce7366f346c
rimlaban7 <a.banjanovic@gmail.com>
2024-02-16 12:41:35 -0500

    Add .gitignore. Modify main.tf.

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 terraform {
+
+  backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "advanced-terraform-almirb40"
+
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "example-workspace"
+         }
+  }
+
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
