bcbba8b2805ce1e8f68940e278f6f8778f77dd94
rimlaban7 <a.banjanovic@gmail.com>
2024-02-19 12:36:54 -0500

    Fix variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "resource_group_name" {
     type = string
+    default = "terra_cloud_github_actions_rg"
 }
 
 variable "location" {
   type = string
+  default = "East Us"
 }
 
 variable "storage_account_name" {
     type = string
+    default = "terra_cloud_github_actions_storage"
 }
 
 variable "function_name" {
     type = string
+    default = "terra_cloud_github_actions_function"
 }
 
 variable "app_service_plan_id" {
