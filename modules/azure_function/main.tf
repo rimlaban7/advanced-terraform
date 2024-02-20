2014a8d96cc7a160b3ccf4b16fac0b41d95d97ef
rimlaban7 <a.banjanovic@gmail.com>
2024-02-19 19:02:04 -0500

    Add storage account module

--- modules/azure_function/main.tf
+++ modules/azure_function/main.tf
@@ -1,10 +1,3 @@
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-
 module "storage_account" {
   source = "./modules/storage_account"
 
